8823439098d1a31dab9cc8b885706c266db7e547
8379K <69079706+8379K@users.noreply.github.com>
2021-12-15 14:49:52 +0900

    shinchoku

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/8379K/oidc-sandbox/handler"
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("hello byebye")
+	fmt.Println("")
+	e := echo.New()
+	e.Static("/", "dist")
+
+	api := e.Group("/api")
+	{
+		apiAccess := api.Group("/acess")
+		{
+			apiAccess.GET("", handler.GetAccess)
+
+		}
+	}
 }
