ef5ad022db657944688a1ad24f9cb4b089a63a9c
8379K <69079706+8379K@users.noreply.github.com>
2021-12-22 14:34:20 +0900

    shinchoku

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,15 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func GetAccess(c echo.Context) error {
+	return c.NoContext(http.StatusNoContent)
+}
+
+func PostAccess(c echo.Context) error {
+	return c.NoContext(http.StatusNoContent)
+}
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/8379K/oidc-sandbox/handler"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -17,7 +19,15 @@ func main() {
 		apiAccess := api.Group("/acess")
 		{
 			apiAccess.GET("", handler.GetAccess)
-
+			apiAccess.POST("", handler.PostAccess)
 		}
 	}
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
 }
